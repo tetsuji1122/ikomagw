d74ecabdbad899e3cafeb4f0d288ed998b205f8a
tetsuji1122 <ishii.gifu403@gmail.com>
2017-01-21 14:37:26 +0900

    KMLの追加

--- Ikome_cesium-starter-app-master/Source/App.js
+++ Ikome_cesium-starter-app-master/Source/App.js
@@ -1,2 +1,3 @@
 var viewer = new Cesium.Viewer('cesiumContainer');
 
+viewer.dataSources.add(Cesium.KmlDataSource.load('https://tetsuji1122.github.io/ikomagw/Ikome_cesium-starter-app-master/Source/Ikoma_sample_matsuda.kml'));
