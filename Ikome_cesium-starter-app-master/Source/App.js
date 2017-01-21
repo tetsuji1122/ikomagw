d0ead7d84ddf97c6d36972959fa21efb7c9422c9
tetsuji1122 <ishii.gifu403@gmail.com>
2017-01-21 15:46:51 +0900

    ikomaデジタルアーカイブのKML追加

--- Ikome_cesium-starter-app-master/Source/App.js
+++ Ikome_cesium-starter-app-master/Source/App.js
@@ -1,3 +1,4 @@
 var viewer = new Cesium.Viewer('cesiumContainer');
 
-viewer.dataSources.add(Cesium.KmlDataSource.load('https://tetsuji1122.github.io/ikomagw/Ikome_cesium-starter-app-master/Source/Ikoma_sample_matsuda.kml'));
+//viewer.dataSources.add(Cesium.KmlDataSource.load('https://tetsuji1122.github.io/ikomagw/Ikome_cesium-starter-app-master/Source/Ikoma_sample_matsuda.kml'));
+viewer.dataSources.add(Cesium.KmlDataSource.load('https://tetsuji1122.github.io/ikomagw/Ikome_cesium-starter-app-master/Source/civictechparty.kml'));
